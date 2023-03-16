29a89daf86028aea7b3d9f4a41b46b2113d8c886
Yasir Shariff <goodwonder5@gmail.com>
2023-03-16 07:35:52 +0300

    🎉  Initial commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Riff
+
+This program is written in Rust and uses the Levenshtein algorithm to find the difference between two file text.
+
+## Usage
+
+To use this program, simply pass in two strings as command line arguments. The program will then output the difference in there two files
+
+``` bash
+./riff ./file1.txt ./file2.txt
+```
+
+## Educational Purpose
+
+This program is intended for educational purposes only. It provides a simple example of how to implement the Levenshtein algorithm in Rust and how it can be used to find the difference between two file texts.
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::path::Path;
+
+use anyhow::anyhow;
+
+fn main() -> anyhow::Result<()> {
+    let args: Vec<_> = std::env::args().collect();
+
+    if args.len() > 3 {
+        return Err(anyhow!("Usage: {} <file> <file>", args[0]));
+    }
+
+    let file_one = read_file(Path::new(&args[1]))?;
+    let file_two = read_file(Path::new(&args[2]))?;
+
+    eprintln!("file contents : {file_two}");
+    Ok(())
+}
+
+fn read_file(file: &Path) -> anyhow::Result<String> {
+    // Read the file
+
+    let file_contents = std::fs::read_to_string(file)?;
+
+    Ok(file_contents)
+}
